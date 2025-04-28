6e72a7c97c384c924150d5addba63dfd9d38f402
H1bbe <hibbe@hibbe.se>
2025-04-28 16:19:16 +0200

    contribute php code checkpoint

--- contribute.php
+++ contribute.php
@@ -1,3 +1,53 @@
+<?php
+session_start();
+require_once 'config.php';
+
+#region Sticky castmembers
+$castcheck = null;
+if (isset($_SESSION['selected_cast']) && !isset($_POST['iid'])) {
+    $selectedcast = $_SESSION['selected_cast'];
+} elseif (isset($_POST['iid'])) {
+    $_SESSION['selected_cast'] = $_POST['iid'];
+    $selectedcast = $_POST['iid']; // Keep this for use *within* the current request
+} else {
+    $selectedcast = [0, 0];
+}
+#endregion
+
+#region Adding and removing video fields
+
+if (isset($_SESSION['videocard_count']) && isset($_SESSION['video_urls']) && !isset($_POST['VidId'])) {
+    // Subsequent load - retrieve from session (but only if NOT a new submission)
+    $videocardcount = $_SESSION['videocard_count'];
+    $storedVideoUrls = $_SESSION['video_urls'];
+} elseif (isset($_POST['VidId'])) { // Form submitted - store and use the new URLs
+    $_SESSION['videocard_count'] = count($_POST['VidId']);
+    $_SESSION['video_urls'] = $_POST['VidId'];
+    $videocardcount = $_SESSION['videocard_count'];
+    $storedVideoUrls = $_POST['VidId']; 
+
+} else {
+    $videocardcount = 0;
+    $storedVideoUrls = []; // Initialize an empty array if no URLs are stored
+}
+
+if ($videocardcount >= 10) {
+    $videocardcount = 10;
+}
+if ($videocardcount <= 0) {
+    $videocardcount = 0;
+}
+
+
+$videocardbool = 1;
+if (isset($_POST['pv'])) {
+    //var_dump($_POST['pv']);
+}
+#endregion
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -21,66 +71,41 @@
         </header>
         <main>
         <a href='/'> << Go back</a>
-            <article class="searchresults">  <!-- Here go the results of the search-->
-                <section class="noselect"> <!-- Search selector --> 
+            <article class="searchresults">  <!-- Not actually search results-->
+                <section class="noselect"> <!-- No Select --> 
                     <div class="card card-contribute">
-                    <form action="contribute.php">
-                        <!--#region castmembers-->
-                        <input type="checkbox" class="hiddencb" id="Lewis"      name="iid[]" value="Lewis">          <label for="Lewis"      class="castcb castcb-contribute lewis">Lewis</label>
-                        <input type="checkbox" class="hiddencb" id="Simon"      name="iid[]" value="Simon">          <label for="Simon"      class="castcb castcb-contribute simon">Simon</label>
-                        <input type="checkbox" class="hiddencb" id="Sips"       name="iid[]" value="Sips">           <label for="Sips"       class="castcb castcb-contribute sips">Sips</label>
-                        <input type="checkbox" class="hiddencb" id="Duncan"     name="iid[]" value="Duncan">         <label for="Duncan"     class="castcb castcb-contribute duncan">Duncan</label>
-                        <input type="checkbox" class="hiddencb" id="Rythian"    name="iid[]" value="Rythian">        <label for="Rythian"    class="castcb castcb-contribute rythian">Rythian</label>
-                        <input type="checkbox" class="hiddencb" id="Ben"        name="iid[]" value="Ben">            <label for="Ben"        class="castcb castcb-contribute ben">Ben</label>
-                        <input type="checkbox" class="hiddencb" id="Angor"      name="iid[]" value="Angor">          <label for="Angor"      class="castcb castcb-contribute angor">Tom</label>
-                        <input type="checkbox" class="hiddencb" id="Zylus"      name="iid[]" value="Zylus">          <label for="Zylus"      class="castcb castcb-contribute zylus">Zylus</label>
-                        <input type="checkbox" class="hiddencb" id="Zoey"       name="iid[]" value="Zoey">           <label for="Zoey"       class="castcb castcb-contribute zoey">Zoey</label>
-                        <input type="checkbox" class="hiddencb" id="Nilesy"     name="iid[]" value="Nilesy">         <label for="Nilesy"     class="castcb castcb-contribute nilesy">Nilesy</label> <!-- 10 -->
-                        <input type="checkbox" class="hiddencb" id="Ravs"       name="iid[]" value="Ravs">           <label for="Ravs"       class="castcb castcb-contribute ravs">Ravs</label>
-                        <input type="checkbox" class="hiddencb" id="Barry"      name="iid[]" value="Barry">          <label for="Barry"      class="castcb castcb-contribute barry">Harry</label>
-                        <input type="checkbox" class="hiddencb" id="Lydia"      name="iid[]" value="Lydia">          <label for="Lydia"      class="castcb castcb-contribute lydia">Lydia</label>
-                        <input type="checkbox" class="hiddencb" id="Bouphe"     name="iid[]" value="Bouphe">         <label for="Bouphe"     class="castcb castcb-contribute bouphe">Bouphe</label>
-                        <input type="checkbox" class="hiddencb" id="Osie"       name="iid[]" value="Osie">           <label for="Osie"       class="castcb castcb-contribute osie">Osie</label>
-                        <input type="checkbox" class="hiddencb" id="Briony"     name="iid[]" value="Briony">         <label for="Briony"     class="castcb castcb-contribute briony">Briony</label>
-                        <input type="checkbox" class="hiddencb" id="Kirsty"     name="iid[]" value="Kirsty">         <label for="Kirsty"     class="castcb castcb-contribute kirsty">Kirsty</label>
-                        <input type="checkbox" class="hiddencb" id="Boba"       name="iid[]" value="Boba">           <label for="Boba"       class="castcb castcb-contribute boba">Boba</label>
-                        <input type="checkbox" class="hiddencb" id="Pedguin"    name="iid[]" value="Pedguin">        <label for="Pedguin"    class="castcb castcb-contribute pedguin">Pedguin</label>
-                        <input type="checkbox" class="hiddencb" id="Trott"      name="iid[]" value="Trott">          <label for="Trott"      class="castcb castcb-contribute trott">Trott</label> <!-- 20 -->
-                        <input type="checkbox" class="hiddencb" id="Smith"      name="iid[]" value="Smith">          <label for="Smith"      class="castcb castcb-contribute smith">Smith</label>
-                        <input type="checkbox" class="hiddencb" id="Ross"       name="iid[]" value="Ross">           <label for="Ross"       class="castcb castcb-contribute ross">Ross</label>
-                        <input type="checkbox" class="hiddencb" id="Hulmes"     name="iid[]" value="Hulmes">         <label for="Hulmes"     class="castcb castcb-contribute hulmes">Mark H</label>
-                        <input type="checkbox" class="hiddencb" id="Daf"        name="iid[]" value="Daf">            <label for="Daf"        class="castcb castcb-contribute daf">Daf</label>
-                        <input type="checkbox" class="hiddencb" id="Breeh"      name="iid[]" value="Breeh">          <label for="Breeh"      class="castcb castcb-contribute breeh">Breeh</label>
-                        <input type="checkbox" class="hiddencb" id="Pyrion"     name="iid[]" value="Pyrion">         <label for="Pyrion"     class="castcb castcb-contribute pyrion">Pyrion</label>
-                        <input type="checkbox" class="hiddencb" id="Daltos"     name="iid[]" value="Daltos">         <label for="Daltos"     class="castcb castcb-contribute daltos">Daltos</label>
-                        <input type="checkbox" class="hiddencb" id="Rambler"    name="iid[]" value="Rambler">        <label for="Rambler"    class="castcb castcb-contribute rambler">Rambler</label>
-                        <input type="checkbox" class="hiddencb" id="Spiff"      name="iid[]" value="Spiff">          <label for="Spiff"      class="castcb castcb-contribute spiff">Spiff</label>
-                        <input type="checkbox" class="hiddencb" id="Martyn"     name="iid[]" value="Martyn">         <label for="Martyn"     class="castcb castcb-contribute martyn">Martyn</label> <!-- 30 -->
-                        <input type="checkbox" class="hiddencb" id="Kim"        name="iid[]" value="Kim">            <label for="Kim"        class="castcb castcb-contribute kim">Kim</label>
-                        <input type="checkbox" class="hiddencb" id="Gee"        name="iid[]" value="Gee">            <label for="Gee"        class="castcb castcb-contribute gee">Gee</label>
-                        <input type="checkbox" class="hiddencb" id="Wilsonator" name="iid[]" value="Wilsonator">     <label for="Wilsonator" class="castcb castcb-contribute wilsonator">Wilsonator</label>
-                        <input type="checkbox" class="hiddencb" id="Ryan"       name="iid[]" value="Ryan">           <label for="Ryan"       class="castcb castcb-contribute ryan">Ryan</label>
-                        <input type="checkbox" class="hiddencb" id="Vadact"     name="iid[]" value="Vadact">         <label for="Vadact"     class="castcb castcb-contribute vadact">Vadact</label>
-                        <input type="checkbox" class="hiddencb" id="Bekki"      name="iid[]" value="Bekki">          <label for="Bekki"      class="castcb castcb-contribute bekki">Bekki</label>
-                        <input type="checkbox" class="hiddencb" id="Mousie"     name="iid[]" value="Mousie">         <label for="Mousie"     class="castcb castcb-contribute mousie">Mousie</label>
-                        <input type="checkbox" class="hiddencb" id="Fionn"      name="iid[]" value="Fionn">          <label for="Fionn"      class="castcb castcb-contribute fionn">Fionn</label>
-                        <input type="checkbox" class="hiddencb" id="Mango"      name="iid[]" value="Mango">          <label for="Mango"      class="castcb castcb-contribute mango">Mango</label>
-                        <input type="checkbox" class="hiddencb" id="Shadow"     name="iid[]" value="Shadow">         <label for="Shadow"     class="castcb castcb-contribute shadow">Shadow</label> <!-- 40 -->
-                        <input type="checkbox" class="hiddencb" id="Lolip"      name="iid[]" value="Lolip">          <label for="Lolip"      class="castcb castcb-contribute lolip">Lolip</label>
-                        <!--#endregion-->
+        <form action="contribute.php" method="post">
+                            <!--#region castmembers-->
+                            <?php 
+                                foreach ($castmember as $icast){ /* iterates castmember icons */
+                                    $castcheck = (isset($_POST['iid']) && in_array($icast, $_POST['iid'])) ? 'checked' : '';
+                            ?>
+                                <input type="checkbox" class="hiddencb" id="<?= $icast ?>"      name="iid[]" value="<?= $icast ?>" <?= $castcheck ?>>          <label for="<?= $icast ?>"      class="castcb castcb-contribute <?= $icast ?>"><?= $icast ?></label>
+                            <?php }; ?>
+                                <!--#endregion-->
                     </div>
                 </section>
-                    <div class="card card-contribute">
-                        <input type="url" name="VidId" id="VidId" placeholder="https://youtube.com/watch?v=" size="25" required title="URL should be a valid Youtube video link" value="https://www.youtube.com/watch?v=G7RxR28Zbco"> <input type="submit" id="PlusVideoRemove" value="＋" />
-                    </div>
-                    <br>
-                    <div class="card card-contribute">
-                    <input type="url" name="VidId" id="VidId" placeholder="https://youtube.com/watch?v=" size="25" required title="URL should be a valid Youtube video link" value="https://www.youtube.com/watch?v=G7RxR28Zbco"> <input type="submit" id="PlusVideoSubmit"  value="＋" />
-                    </div>
-                <p> <!-- Here goes the search button -->
+                    <?php   
+                        $j = $videocardcount;
+                        for($i = 0; $i <= $j; $i++) { 
+                        $videocardcount+1; 
+                    ?>
+                        <div class="card card-contribute">
+                        <input type="url" name="VidId[]" id="VidId" placeholder="https://youtube.com/watch?v=" size="25" required title="URL should be a valid Youtube video link" value="<?= isset($storedVideoUrls[$i]) ? htmlspecialchars($storedVideoUrls[$i]) : '' ?>"> 
+                        <button formnovalidate class="button" <?php if ($i < $j) { echo 'id="pvr"';}; ?> name="pv" value="<?= $i ?>" type="submit">＋</button>
+                        </div> 
+                        <br>
+                    <?php    };?>
+                        
+
+
+
+
+
+                <p> <!-- Here goes the submit button -->
                     <input type="submit" id="Searchsubmit" value="Submit" />
                 </p>
-                    </form>
+        </form>
 
 
             </article>
